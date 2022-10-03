2106dfb3d869ad25e40c472e09bf8e41ac03e103
Takahiro <aibolife.mail@gmail.com>
2022-10-03 15:18:51 +0900

    ログ出力修正

--- app/Providers/DataBaseQueryServiceProvider.php
+++ app/Providers/DataBaseQueryServiceProvider.php
@@ -47,7 +47,7 @@ class DataBaseQueryServiceProvider extends ServiceProvider
                 $sql = preg_replace('/\\?/', $binding, $sql, 1);
             }
 
-            $ip = $_SERVER["REMOTE_ADDR"];
+            $ip = Request::ip();
 
             Log::channel('sql')->debug('SQL', ['sql' => $sql, 'IP' => $ip]);
         });
@@ -75,17 +75,4 @@ class DataBaseQueryServiceProvider extends ServiceProvider
         //
     }
 
-    // //追加
-    // public function getIp(){
-    //     foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-    //         if (array_key_exists($key, $_SERVER) === true){
-    //             foreach (explode(',', $_SERVER[$key]) as $ip){
-    //                 $ip = trim($ip); // just to be safe
-    //                 if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-    //                     return $ip;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 }
