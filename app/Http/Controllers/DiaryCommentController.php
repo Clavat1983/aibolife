d8f4e367c717ffe0a9a401e0c354f83fa78d8ea8
Takahiro <aibolife.mail@gmail.com>
2022-11-21 16:14:30 +0900

    SQL修正

--- app/Http/Controllers/DiaryCommentController.php
+++ app/Http/Controllers/DiaryCommentController.php
@@ -138,7 +138,8 @@ class DiaryCommentController extends Controller
         if((auth()->user()->owner != NULL) && (auth()->user()->owner->aibos->firstWhere('aibo_available_flag', true) != NULL)){
             $owner_id = auth()->user()->owner->id;
             //コメントを付けた日記のIDを重複なしで、ページネーションありで取得
-            $comments = DiaryComment::select('diary_id')->where('owner_id', $owner_id)->groupBy('diary_id')->orderBy('created_at', 'desc')->paginate(10);
+            $comments = DiaryComment::select('diary_id','created_at')->where('owner_id', $owner_id)->orderby('created_at', 'desc')->orderby('diary_id','desc')->paginate(10);
+
 
             //【全ビュー共通処理】未読通知数
             $bell_count = Notification::where('user_id', auth()->user()->id)->where('read_at', NULL)->count();
